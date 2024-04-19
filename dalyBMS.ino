8a0f21bfb8b9955db6622800a215ff1a0c4dfee6
sidharthn13 <sidharth4network@gmail.com>
2024-04-19 12:24:26 +0530

    Successfully initialized CAN in loopback mode and received data. Have to implement onReceive callback

--- dalyBMS.ino
+++ dalyBMS.ino
@@ -14,7 +14,12 @@ void setup(){
     Serial.println("Initialized CAN");
   }
 
-  //populating the buffer
+  //settign the CAN ports in loopback mode
+  CAN.loopback();
+  //setting up the filter to receive messages:
+  // CAN.filterExtended(0x18900140);
+
+  //populating the Tx buffer
   buffer[0] = 0x00;
   buffer[1] = 0x01;
   buffer[2] = 0x02;
@@ -26,10 +31,32 @@ void setup(){
 }
 
 void loop(){
+  //sending packet
   CAN.beginExtendedPacket(0x18900140,8);
   int numBytesWritten = CAN.write(buffer, 8);
   Serial.print("Num bytes written to CAN frame: ");
   Serial.println(numBytesWritten);
   CAN.endPacket();
+
+  delay(1000);
+
+  //receiving sent packet
+  int packetSize = CAN.parsePacket();
+  Serial.print("Num bytes received in packet: ");
+  Serial.println(packetSize);
+  uint32_t id = CAN.packetId();
+  Serial.print("packet ID: ");
+  Serial.println(id);
+
+  // Serial.println(CAN.available());
+
+  Serial.print("data in packet: ");
+  while(CAN.available()){
+    int data = CAN.read();
+    Serial.print(data);
+    Serial.print(", ");
+  }
+  Serial.println();
+  delay(3000);
 
 }
