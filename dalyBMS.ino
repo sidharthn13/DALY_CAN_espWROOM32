de7a865f56d95930361bf4deb130c6b4f9a54d13
sidharthn13 <sidharth4network@gmail.com>
2024-04-19 17:56:33 +0530

    implemented feature to mimic BMS data

--- dalyBMS.ino
+++ dalyBMS.ino
@@ -1,7 +1,18 @@
 #include<CAN.h>
-  
+
+//initializing buffers to hold receiver data
+typedef struct RxBuffers{
+  uint8_t SocVoltage[8];
+  uint8_t monomerVoltage[8];
+  uint8_t MosStatus[8];
+}RxBuffers;
+RxBuffers rxBuffers;
+
 //initializing buffer to write to the CAN packet
 uint8_t buffer[8];
+
+//initializing array to hold sample data
+uint8_t sampleData[4][8];
 
 void setup(){
   Serial.begin(115200);
@@ -28,29 +39,46 @@ void setup(){
   buffer[5] = 0x00;
   buffer[6] = 0x00;
   buffer[7] = 0x00;
+
+  //populating sample data array
+  uint8_t socVoltage[8] = {0x01, 0x57, 0x00, 0x00, 0x75, 0x4E, 0x03, 0xE8};
+  memcpy(sampleData[0], socVoltage, 8);
+
+  uint8_t monomerVoltage[8] = {0x0C, 0xEE, 0x04, 0x00, 0x00, 0x02, 0x03, 0xE8};
+  memcpy(sampleData[1], monomerVoltage, 8);
+
+  uint8_t MosStatus[8] = {0x42, 0x01, 0x42, 0x01, 0x00, 0x02, 0x03, 0xE8};
+  memcpy(sampleData[2], MosStatus, 8);
 }
 
+//ISR handler that is fired when data is received
 void onReceive(int packetSize) {
-  Serial.print("ID : ");
-  Serial.println(CAN.packetId(), HEX);
-  // Serial.print("data in packet: ");
-  // for(int i = 0; i < 8; i++){
-  //   int data = CAN.read();
-  //   Serial.print(data);
-  //   Serial.print(", ");
-  // }
-  // Serial.println();
+  switch( (CAN.packetId()>>16) & 0xFF ){
+    case 0x90:
+      memcpy(rxBuffers.SocVoltage, sampleData[0], 8);
+      break;
+    case 0x91:
+      memcpy(rxBuffers.monomerVoltage, sampleData[0], 8);
+      break;
+    case 0x92:
+      memcpy(rxBuffers.MosStatus, sampleData[0], 8);
+      break;
+  }
 }
-
+//pass data ID of the desired battery data
 void requestData(uint8_t dataID){
   uint32_t ID = 0x18000140 | (dataID<<16);
   CAN.beginExtendedPacket(ID, 8);
   CAN.write(buffer, 8);
   CAN.endPacket();
 }
-
 void loop(){
   delay(100);
-  //sending packet
+  //sending data requests
   requestData(0x90);
+  for(int i = 0; i < 8; i++){
+    Serial.print(rxBuffers.SocVoltage[i], HEX);
+    Serial.print(", ");
+  }
+  Serial.println();
 }
