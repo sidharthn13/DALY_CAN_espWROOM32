0c95f942cef524d81c45061d4f7c5b1347680ea8
sidharthn13 <sidharth4network@gmail.com>
2024-04-22 12:10:40 +0530

    connected CAN BMS to transceiver. Able to receive data packets

--- dalyBMS.ino
+++ dalyBMS.ino
@@ -8,9 +8,6 @@ RxBuffers rxBuffers;
 //initializing buffer to write to the CAN packet
 uint8_t buffer[8];
 
-//initializing array to hold sample data
-uint8_t sampleData[4][8];
-
 void setup(){
   Serial.begin(115200);
 
@@ -20,9 +17,6 @@ void setup(){
   else{
     Serial.println("Initialized CAN");
   }
-
-  //settign the CAN ports in loopback mode
-  CAN.loopback();
 
   //setting calback function to be called when packet is received
   CAN.onReceive(onReceive);
@@ -36,16 +30,6 @@ void setup(){
   buffer[5] = 0x00;
   buffer[6] = 0x00;
   buffer[7] = 0x00;
-
-  //populating sample data array
-  uint8_t socVoltage[8] = {0x01, 0x57, 0x00, 0x00, 0x75, 0x4E, 0x03, 0xE8};
-  memcpy(sampleData[0], socVoltage, 8);
-
-  uint8_t monomerVoltage[8] = {0x0C, 0xEE, 0x04, 0x00, 0x00, 0x02, 0x03, 0xE8};
-  memcpy(sampleData[1], monomerVoltage, 8);
-
-  uint8_t MosStatus[8] = {0x01, 0x01, 0x00, 0x0F, 0x00, 0x00, 0x75, 0x30};
-  memcpy(sampleData[3], MosStatus, 8);
 }
 
 
@@ -53,13 +37,13 @@ void setup(){
 void onReceive(int packetSize) {
   switch( (CAN.packetId()>>16) & 0xFF ){
     case 0x90:
-      memcpy(rxBuffers.SocVoltage, sampleData[0], 8);
+      Serial.println("packet with data ID : 0x90 received");
       break;
     case 0x91:
-      memcpy(rxBuffers.monomerVoltage, sampleData[1], 8);
+      Serial.println("packet with data ID : 0x91 received");
       break;
     case 0x93:
-      memcpy(rxBuffers.MosStatus, sampleData[3], 8);
+      Serial.println("packet with data ID : 0x93 received");
       break;
   }
 }
@@ -75,8 +59,8 @@ void loop(){
   delay(100);
   //sending data requests
   requestData(0x91);
-  processBmsData(0x91);
+  // processBmsData(0x91);
 
   requestData(0x93);
-  processBmsData(0x93);
+  // processBmsData(0x93);
 }
